d40a11ba4994e7682c4197ec983d4e3af0b758e3
Reazul Hoque <rhoque@atlantis.nomad.utk.edu>
2017-01-03 15:33:29 -0500

    added missing curl

--- setup_new_machine.sh
+++ setup_new_machine.sh
@@ -1,4 +1,4 @@
- ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
